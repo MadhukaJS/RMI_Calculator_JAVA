6e9a6c183c90b3d1d80d54300e5aff46dda8e8bc
Madhuka Priyashan <111411737+Madhuka164@users.noreply.github.com>
2023-02-25 01:44:48 +0530

    client
    locate the registry
    
    locate the remote object
    
    ready for invoke methods

--- MyRMICalculator/src/com/ictech/client/MyClient.java
+++ MyRMICalculator/src/com/ictech/client/MyClient.java
@@ -1,7 +1,19 @@
 package com.ictech.client;
 
+import com.ictech.calc.Calculator;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class MyClient {
     public static void main(String[] args) {
+        //locate the registry
+        Registry myReg= LocateRegistry  .getRegistry("localhost",54321);
+
+        //locate the remote object
+        Calculator myCal=(Calculator) myReg.lookup("My Calculator server") ;
+        
+        //invoke the methods
 
     }
 }
