df21330aeed27befc485055898f3195b0705ca21
Madhuka Priyashan <111411737+Madhuka164@users.noreply.github.com>
2023-02-26 23:45:31 +0530

    remote interface
    updated

--- MyRMITemperatureConverter/src/com/ictech/conv/Converter.java
+++ MyRMITemperatureConverter/src/com/ictech/conv/Converter.java
@@ -1,4 +1,15 @@
 package com.ictech.conv;
 
-public interface Converter {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Converter extends Remote {
+
+    public int CelToFar(int x) throws RemoteException;
+
+    public int FartoCel(int x) throws RemoteException;
+
+    public int CeltoKel(int x) throws RemoteException;
+
+    public int KeltoCel(int x) throws RemoteException;
 }
