b4a2cf12856fb12f1d5725752f33ecd96457122c
Madhuka Priyashan <madhuka164@gmail.com>
2023-03-12 23:55:44 +0530

    fixing server

--- MyRMIAreaFinder/src/com/ictech/server/MyServer.java
+++ MyRMIAreaFinder/src/com/ictech/server/MyServer.java
@@ -1,7 +1,21 @@
 package com.ictech.server;
 
+import com.ictech.Areaf.AreaFinder;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class MyServer {
     public static void main(String[] args) {
-
+        try {
+        
+        myReg= LocateRegistry.createRegistry("54321");
+        myFinder = new AreaFinder();
+        myReg.rebind("My area finder server",myFinder);
+        }
+        catch (RemoteException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
