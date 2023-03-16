8625410b68882c089ed69bef16f7baac6d3421a9
Madhuka Priyashan <madhuka164@gmail.com>
2023-03-16 23:42:04 +0530

    update

--- MyAgeCalculator/src/com/ictech/AgeCal/MyAgeCalculator.java
+++ MyAgeCalculator/src/com/ictech/AgeCal/MyAgeCalculator.java
@@ -1,4 +1,9 @@
 package com.ictech.AgeCal;
 
-public class MyAgeCalculator {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MyAgeCalculator extends UnicastRemoteObject implements AgeCalculator {
+    public MyAgeCalculator() throws RemoteException {
+    }
 }
