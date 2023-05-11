45973efb9c9143c4021117bd06d8e3a3140cbbd4
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-05-12 02:12:32 +0530

    updated

---
+++ RMICalculator02/src/com/ictec/MyCalculator.java
@@ -0,0 +1,11 @@
+package com.ictec;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MyCalculator extends UnicastRemoteObject implements calculator {
+    protected MyCalculator() throws RemoteException {
+    }
+
+    
+}
---
+++ RMICalculator02/src/com/ictec/calculator.java
@@ -0,0 +1,6 @@
+package com.ictec;
+
+import java.rmi.Remote;
+
+public interface calculator extends Remote {
+}
