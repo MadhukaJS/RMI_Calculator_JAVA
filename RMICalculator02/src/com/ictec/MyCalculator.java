24cfe8b4c4e541902340aec12682dfb88189936f
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-05-13 19:28:54 +0530

    interface done!

--- RMICalculator02/src/com/ictec/MyCalculator.java
+++ RMICalculator02/src/com/ictec/MyCalculator.java
@@ -3,9 +3,8 @@ package com.ictec;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class MyCalculator extends UnicastRemoteObject implements calculator {
-    protected MyCalculator() throws RemoteException {
-    }
+public class MyCalculator {
+
 
 
 }
--- RMICalculator02/src/com/ictec/calculator.java
+++ RMICalculator02/src/com/ictec/calculator.java
@@ -1,6 +1,13 @@
 package com.ictec;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface calculator extends Remote {
+
+    public int addtwointeger (int x,int y) throws RemoteException;
+    public int devTwointeger (int x,int y) throws RemoteException;
+
+    public int subTwoInteger (int x,int y) throws RemoteException;
+    public int mulTwoInteger (int x,int y) throws RemoteException;
 }
